4c8f4337131aba448180ca4631a6ea69a8d3e235
shinn_lancelot <lanhaichun0317@gmail.com>
2019-07-18 15:48:05 +0800

    1. 修改App.vue为app.vue

---
+++ examples/app.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <mf-nav-bar>
+      <template slot="title">
+        这是一个导航组件，目前固定于顶部。
+      </template>
+    </mf-nav-bar>
+    <mf-loading :show="loading.show" :type="loading.type"></mf-loading>
+    <mf-progress-bar></mf-progress-bar>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'App',
+    data () {
+      return {
+        loading: {
+          show: true,
+          type: 1
+        }
+      }
+    },
+    mounted () {
+      let _this = this
+      setTimeout(() => {
+        _this.loading.show = false
+      }, 3000)
+    }
+  }
+</script>
